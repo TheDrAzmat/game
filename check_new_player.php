6720228f941c168d42207860a16d45e88c0c7f43
Stunning Hub <60594587+stunninghub@users.noreply.github.com>
2020-11-01 18:05:18 +0530

    New Files

---
+++ check_new_player.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include "db.php";
+$current_date = (floor(gettimeofday(true))-3);
+
+$sql = "SELECT * FROM `players` WHERE room = '".$_SESSION['room']."' and online_status >= '$current_date'";
+$result = mysqli_query($con,$sql);
+$row_num = mysqli_num_rows($result);
+
+echo $row_num;
+
+?>
---
+++ create_player.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+include "db.php";
+$uname = $_REQUEST['uname'];
+$user_agent = getenv("HTTP_USER_AGENT");
+
+$n=4; 
+function getName($n) { 
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    $randomString = ''; 
+  
+    for ($i = 0; $i < $n; $i++) { 
+        $index = rand(0, strlen($characters) - 1); 
+        $randomString .= $characters[$index]; 
+    } 
+  
+    return $randomString; 
+}
+$u_id = strtolower($uname.getName($n));
+
+$sql_check ="SELECT * FROM `players` WHERE u_id = '$u_id'";
+$result = mysqli_query($con,$sql_check);
+$row_num = mysqli_num_rows($result);
+
+if($row_num <=0 ){
+    $sql = "INSERT INTO `players` (`uname`,`u_id`,`u_char`,`user_agent`, `room`) VALUES ('$uname','$u_id','','$user_agent','') ";
+    if(mysqli_query($con,$sql)){
+        $_SESSION['uname']=$uname;
+        $_SESSION['u_id']=$u_id;
+        echo "ok";
+    }else{
+        echo "SQL Error";
+    }
+}else{  
+    echo "User already exist";
+}
+?>
